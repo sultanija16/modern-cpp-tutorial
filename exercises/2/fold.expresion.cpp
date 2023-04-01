7bed4182a9176594b36c1dc5a506ad40a2b620b8
H <103602300+tidzaa@users.noreply.github.com>
2023-04-01 18:14:44 -0400

    Update fold.expresion.cpp

--- exercises/2/fold.expresion.cpp
+++ exercises/2/fold.expresion.cpp
@@ -14,5 +14,5 @@ auto average(T ... t) {
     return (t + ... ) / sizeof...(t);
 }
 int main() {
-    std::cout << average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) << std::endl;
+    std::cout << average(1, 2, 3, 4, 5, 6, 7, 8, 9, 10) << std:: endl;
 }
